56e2e338d68392fef46b47abbc3c722b47cd071c
Joel Lonneker <joel.lonneker@gmail.com>
2018-06-09 08:46:49 -0700

    non functional.  Running through steps at jasonformat.com/wtf-is-jsx

---
+++ hyperscript.js
@@ -0,0 +1,63 @@
+// writing the JSX renderer with hyperscript
+
+function h(nodeName, attributes, ...args) {
+    let children = args.length ? [].concat(...args) : null;
+    return (nodeName, attributes, children);
+}
+
+function render(vnode) {
+    // Strings convert to #text Nodes
+    if (vnode.split) return document.createTextNode(vnode);
+
+    // create a DOM element with the nodeName of our VDOM element
+    let n = document.createElement(vnode.nodeName);
+
+    // copy attributes onto the new node
+    let a = vnode.attributes || {};
+    Object.keys(a).forEach( k => n.setAttribute(k, a[k]) );
+    
+    // render (build) and then append child nodes
+    (vnode.children || []).forEach( c => n.appendChild(render(c)) );
+
+    return n;
+}
+
+// // JSX -> VDOM
+// let vdom = <div id="foo">Hello!</div>;
+
+// // VDOM -> DOM
+// let dom = render(vdom);
+
+// // add the tree to <body>
+// document.body.appendChild(dom);
+
+const ITEMS = 'hello there people'.split(' ');
+
+// turn an Array into list items: 
+let list = items => items.map( p => <li> {p} </li> );
+
+// view with a call out ("partial") to generate a list from an Array:
+let vdom = (  
+    <div id="foo">
+        <p>Look, a simple JSX DOM renderer!</p>
+        <ul>{ list(ITEMS) }</ul>
+    </div>
+);
+
+// render() converts our "virtual DOM" (see below) to a real DOM tree:
+let dom = render(vdom);
+
+// append the new nodes somewhere:
+document.body.appendChild(dom);
+
+// Remember that "virtual DOM"? It's just JSON - each "VNode" is an object with 3 properties.
+let json = JSON.stringify(vdom, null, '  ');
+
+// The whole process (JSX -> VDOM -> DOM) in one step:
+document.body.appendChild(  
+    render( <pre id="vdom">{ json }</pre> )
+);
+
+
+// https://codepen.io/developit/pen/aOYywe
+
